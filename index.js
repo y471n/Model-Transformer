836d18b623a815fa80952bcaa644ed68ed53406a
RastogiAbhijeet <abhijeet.rastogi@block8.io>
2019-05-21 22:35:05 +0530

    Keep Remaining Fields

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-// Suppose we get an object with the given nature from the database:
-const { get, set } = require("lodash");
-
-class Map {
-  constructor(current, target = null, func = args => args) {
-    if (current) {
-      this._current = current;
-
-      // the below block could be avoided but has been included to make the class more readable
-      if (target === null) {
-        target = current;
-      }
-
-      this._target = target;
-      this._func = func;
-    } else {
-      // This exception will be thrown when the current key is missing
-      throw new Error("Model Key Missing");
-    }
-  }
-}
-
-class Transform {
-  constructor() {
-    this._result = {};
-  }
-
-  transform(_object_arr, model) {
-    _object_arr.map(object => {
-      set(
-        this._result,
-        object._target,
-        object._func(get(model, object._current))
-      );
-    });
-
-    return this._result;
-  }
-}
+const { Transform, Map } = require("./src/transformer");
 
 let model = {
   _id: 1,
@@ -63,6 +25,5 @@ const result = new Transform().transform(mappings, model);
 console.log(result);
 
 // TODO
-// - leave keys FLAG
 // - try to identify the array and transform all the object
 // - type Conversion
