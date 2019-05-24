be9676f099082585e271b6d76176dc9fa5a4bf78
RastogiAbhijeet <abhijeet.rastogi@block8.io>
2019-05-24 15:23:40 +0530

    Refactoring the architecture

---
+++ packages/model-transformer/rollup.config.js
@@ -0,0 +1,49 @@
+import svgr from "@svgr/rollup";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import replace from "rollup-plugin-replace";
+import typescript from "rollup-plugin-typescript2";
+import url from "rollup-plugin-url";
+import pkg from "./package.json.js";
+
+const input = "src/index.ts";
+
+const plugins = [
+  external(),
+  replace({
+    "process.env.NODE_ENV": JSON.stringify(
+      process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : "development"
+    )
+  }),
+  url(),
+  svgr(),
+  resolve(),
+  typescript({
+    rollupCommonJSResolveHack: true,
+    clean: true,
+    tsconfig: "./tsconfig.json"
+  }),
+  commonjs()
+];
+
+export default [
+  {
+    input,
+    output: {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named"
+    },
+    plugins
+  },
+  {
+    input,
+    output: {
+      file: pkg.module,
+      format: "es",
+      exports: "named"
+    },
+    plugins
+  }
+];
